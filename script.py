f944fb0026f9775679753e951c969db21dae808f
cuongvu_qi <cuongvu@quangich.com>
2020-06-07 16:32:19 +0700

    sửa lại phần script

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import csv
 import time
 import configparser
 
+#test push github aaaa
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 driver.get('https://shub.edu.vn/login')
